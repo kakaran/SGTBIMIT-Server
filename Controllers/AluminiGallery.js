624537fdb0d3339f147a821068964d0122392621
Utkp5 <utkp200@gmail.com>
2023-04-15 02:27:42 +0530

    alumini cntrl routes model chng

--- Controllers/AluminiGallery.js
+++ Controllers/AluminiGallery.js
@@ -14,7 +14,7 @@ const aluminiAddImage = async(req,res) => {
         }else if (!category) {
             return res.status(401).send(`category must be defined`);            
         }
-        
+
         const already = await aluminiModel.findOne({category})
 
         if (already) {
@@ -63,85 +63,86 @@ const aluminiAddImage = async(req,res) => {
 
 
 //alumini image update
-const aluminiUpdateImage = async(req,res) => {
+// const aluminiUpdateImage = async(req,res) => {
 
-    try {
-        const {category} = req.fields;
-        const {image} = req.files;
-        const id = req.params._id
+//     try {
+//         const {category} = req.fields;
+//         const {image} = req.files;
+//         const id = req.params._id
 
-        if (!image || image.size > 1000000) {
-            return res.status(401).send(`image is req or image size should be less then 1MB`);
-        }else if (!category) {
-            return res.status(401).send(`category must be defined`);            
-        }
+//         if (!image || image.size > 1000000) {
+//             return res.status(401).send(`image is req or image size should be less then 1MB`);
+//         }else if (!category) {
+//             return res.status(401).send(`category must be defined`);            
+//         }
 
 
-        const alumini = await aluminiModel.findById(id);
+//         const alumini = await aluminiModel.findById(id);
 
-        if (alumini) {
-            const alumini_g  = await aluminiModel.findByIdAndUpdate(alumini, {...req.fields }, { new: true });
-            alumini_g.image.data = fs.readFileSync(image.path),
-            alumini_g.image.contentType = image.type   
-        }
+//         if (alumini) {
+//             const alumini_g  = await aluminiModel.findByIdAndUpdate(alumini, {...req.fields }, { new: true });
+//             alumini_g.image.data = fs.readFileSync(image.path),
+//             alumini_g.image.contentType = image.type   
+//         }
 
-        // const final = await alumini_g.save();
+//         // const final = await alumini_g.save();
 
-        return res.status(200).send({message : `Updated successfully`});
+//         return res.status(200).send({message : `Updated successfully`});
     
         
-    } catch (error) {
-        console.log(error);
-        return res.status(500).send({
-            success: false,
-            message: 'Error'
-        })
-    }
-}
+//     } catch (error) {
+//         console.log(error);
+//         return res.status(500).send({
+//             success: false,
+//             message: 'Error'
+//         })
+//     }
+// }
 
 
 //alumini carousel image
-const aluminiAddCarouselImage = async(req,res) => {
+// const aluminiAddCarouselImage = async(req,res) => {
 
-    try {
+//     try {
 
-        const {images} = req.files
+//         const {images} = req.files
 
-        if (!images || images.size > 1000000) {
-            return res.status(401).send(`image is req or image size should be less then 1MB`);
-        }
+//         if (!images || images.size > 1000000) {
+//             return res.status(401).send(`image is req or image size should be less then 1MB`);
+//         }
 
-        const alumini = await new aluminiModel(req.fields);
+//         const alumini = await new aluminiModel(req.fields);
 
-        if (images.length) {
-            for (let i = 0; i < images.length; i++) {
-                alumini.images.push({
-                    data: fs.readFileSync(images[i].path),
-                    contentType: images[i].type,
-                })
-            }
-        }else {
-            alumini.images.push({
-                data: fs.readFileSync(images.path),
-                contentType: images.type,
-            })
-        }
+//         if (images.length) {
+//             for (let i = 0; i < images.length; i++) {
+//                 alumini.images.push({
+//                     data: fs.readFileSync(images[i].path),
+//                     contentType: images[i].type,
+//                 })
+//             }
+//         }else {
+//             alumini.images.push({
+//                 data: fs.readFileSync(images.path),
+//                 contentType: images.type,
+//             })
+//         }
         
-        const final = await alumini.save();
+//         const final = await alumini.save();
 
-        return res.status(200).send({
-            message : `Images uploaded successfully`,
-            final
-        });
+//         return res.status(200).send({
+//             message : `Images uploaded successfully`,
+//             final
+//         });
 
 
 
-    } catch (error) {
-        console.log(error);
-        return res.status(500).send({
-            success: false,
-            message: 'Error'
-        })
-    }
-}
+//     } catch (error) {
+//         console.log(error);
+//         return res.status(500).send({
+//             success: false,
+//             message: 'Error'
+//         })
+//     }
+// }
+
 module.exports = {aluminiAddImage, aluminiUpdateImage, aluminiAddCarouselImage} 
