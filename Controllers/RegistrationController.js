d366dbe7fae4e25490ee779bc30883865c74db6e
Jatin Ahluwalia <jatin.ahluwalia.5@gmail.com>
2023-05-21 04:21:40 +0530

    Registeration form

--- Controllers/RegistrationController.js
+++ Controllers/RegistrationController.js
@@ -17,7 +17,7 @@ const Registration = require('../Models/Registration');
 //             subject: 'Admission or joining regarding enquiry/query',
 //             html: `<!DOCTYPE html>
 //         <html lang="en">
-        
+
 //         <head>
 //             <title></title>
 //             <style>
@@ -29,7 +29,7 @@ const Registration = require('../Models/Registration');
 //                     width: 50%;
 //                     /* margin: auto; */
 //                 }
-        
+
 //                 table {
 //                     margin: 20px;
 //                     font-size: 20px;
@@ -37,11 +37,11 @@ const Registration = require('../Models/Registration');
 //                     text-transform: capitalize;
 //                     border: 1px solid #d9d9d9;
 //                 }
-        
+
 //                 .Container p, pre{
 //                     font-size: 18px;
 //                 }
-        
+
 //                 th,
 //                 td {
 //                     border: 1px solid #4a4e69;
@@ -49,7 +49,7 @@ const Registration = require('../Models/Registration');
 //                 }
 //             </style>
 //         </head>
-        
+
 //         <body>
 //             <div class="Container">
 //                 <p>Dear ${Name}</p>
@@ -81,7 +81,7 @@ const Registration = require('../Models/Registration');
 //                 <p>SGTBIMIT</p>
 //             </div>
 //         </body>
-        
+
 //         </html>`
 //             // text: `${otp} is your One-Time Password (OTP) to complete reset your password.`,
 //         };
@@ -101,68 +101,73 @@ const Registration = require('../Models/Registration');
 
 const RegistrationAdd = async (req, res) => {
     try {
-        const { Fname, Lname, Email, MNumber, Address, AdhaarNo, course, Year, employed, placement, presentOrgani, CurrentDesignation } = req.body;
+        const { Fname, Lname, Email, MNumber, Address, AdhaarNo, Gender, course, Year, employed, placement, presentOrgani, CurrentDesignation } = req.body;
+        console.log(req.body);
 
-        if(!Fname){
+        if (!Fname) {
             return res.status(401).send("First Name is required");
-        }else if(!Lname){
+        } else if (!Lname) {
             return res.status(401).send("Last Name is required");
-        }else if(!Email){
+        } else if (!Gender) {
+            return res.status(401).send("Gender is required");
+        } else if (!Email) {
             return res.status(401).send("Emailis is required");
-        }else if(!MNumber){
+        } else if (!MNumber) {
             return res.status(401).send("Mobile Number is required");
-        }else if(!Address){
+        } else if (!Address) {
             return res.status(401).send("Address is required");
-        }else if(!AdhaarNo){
+        } else if (!AdhaarNo) {
             return res.status(401).send("Adhaar No is required");
-        }else if(!course){
+        } else if (!course) {
             return res.status(401).send("Course is required");
-        }else if(!Year){
+        } else if (!Year) {
             return res.status(401).send("Year is  required");
-        }else if(!employed){
+        } else if (!employed) {
             return res.status(401).send("Employed Detail is required");
         }
 
-        const RegistrationCheck = await Registration.find({Email : Email});
-        if(!RegistrationCheck.length){
+        const RegistrationCheck = await Registration.find({ Email: Email });
+        if (!RegistrationCheck.length) {
             const RegistrationData = await Registration({
-                Fname :Fname,
-                Lname :Lname,
-                Email :Email,
-                MNumber :MNumber,
-                Address :Address,
-                AdhaarNo : AdhaarNo,
-                HigerEducation :{
-                    course : course,
-                    Year : Year,
-                    employed :employed
+                Fname: Fname,
+                Lname: Lname,
+                Email: Email,
+                MNumber: MNumber,
+                Address: Address,
+                AdhaarNo: AdhaarNo,
+                Gender: Gender,
+                HigerEducation: {
+                    course: course,
+                    Year: Year,
+                    employed: employed
                 },
-                CurrentWorking :{
-                    placement : placement,
-                    presentOrgani :presentOrgani,
-                    CurrentDesignation :CurrentDesignation
+                CurrentWorking: {
+                    placement: placement,
+                    presentOrgani: presentOrgani,
+                    CurrentDesignation: CurrentDesignation
                 }
             });
-    
+
             await RegistrationData.save();
-    
+
             return res.status(200).send("Registration Successfully Submit")
-        }else {
-            return res.status(200).send("Your are allready Submited")
+        } else {
+            return res.status(500).send("Your are allready Submited")
         }
-        
+
     } catch (error) {
         console.log(error);
+        return res.status(500).send("There was some error, please check your details")
     }
 }
 
-const RegistrationDisplayAll = async (req,res) =>{
+const RegistrationDisplayAll = async (req, res) => {
     try {
         const data = await Registration.find();
 
-        if(data){
+        if (data) {
             return res.status(200).send(data);
-        }else{
+        } else {
             return res.status(404).send("Data not found")
         }
     } catch (error) {
@@ -170,12 +175,12 @@ const RegistrationDisplayAll = async (req,res) =>{
     }
 }
 
-const RegistrationDelete = async (req,res) =>{
+const RegistrationDelete = async (req, res) => {
     try {
-        const {_id} = req.params
-        const data = await Registration.find({_id});
-        if(data){
-            await Registration.findByIdAndDelete({_id});
+        const { _id } = req.params
+        const data = await Registration.find({ _id });
+        if (data) {
+            await Registration.findByIdAndDelete({ _id });
             return res.status(200).send("Deleted")
         }
     } catch (error) {
@@ -184,7 +189,7 @@ const RegistrationDelete = async (req,res) =>{
 }
 
 
-module.exports ={
+module.exports = {
     RegistrationAdd,
     RegistrationDisplayAll,
     RegistrationDelete
--- Models/Registration.js
+++ Models/Registration.js
@@ -1,67 +1,72 @@
 const mongoose = require('mongoose');
 
 const RegistrationSchema = mongoose.Schema({
-    Fname :{
-        type : String,
-        required : true
+    Fname: {
+        type: String,
+        required: true
     },
-    Lname :{
-        type : String,
-        required : true
+    Lname: {
+        type: String,
+        required: true
     },
-    Email : {
-        type : String,
-        required : true
+    Email: {
+        type: String,
+        required: true
     },
-    MNumber : {
-        type : Number,
-        required : true,
+    MNumber: {
+        type: Number,
+        required: true,
         // max : 11,
     },
-    Address :{
-        type : String,
-        required : true,
-        
+    Address: {
+        type: String,
+        required: true,
+
     },
-    AdhaarNo :{
-        type : Number ,
-        required : true,
+    AdhaarNo: {
+        type: Number,
+        required: true,
         // max : 13,
     },
-    HigerEducation : {
-        course : {
-            type : String,
-            required : true,
+    Gender: {
+        type: String,
+        required: true,
+        enum: ["Male", "Female", "Not Prefer"]
+    },
+    HigerEducation: {
+        course: {
+            type: String,
+            required: true,
             enum: ["BCA", "BBA", "BBA B&I", "BCOM"]
         },
-        Year : {
-            type : Number,
-            required :true,
+        Year: {
+            type: Number,
+            required: true,
             // max : 4
         },
-        employed :{
-            type : String,
-            enum : ["Yes","No"]
+        employed: {
+            type: String,
+            enum: ["Yes", "No"]
         }
     },
-    CurrentWorking :{
-        placement  : {
-            type : String,
-            enum : ["Yes" , "No"]
+    CurrentWorking: {
+        placement: {
+            type: String,
+            enum: ["Yes", "No"]
         },
-        presentOrgani : {
-            type : String,
+        presentOrgani: {
+            type: String,
         },
-        CurrentDesignation : {
-            type : String,
+        CurrentDesignation: {
+            type: String,
         }
     }
-},{
-    timestamps : true
+}, {
+    timestamps: true
 });
 
 
-const Registration = mongoose.model("Registration",RegistrationSchema);
+const Registration = mongoose.model("Registration", RegistrationSchema);
 
 
 module.exports = Registration;
