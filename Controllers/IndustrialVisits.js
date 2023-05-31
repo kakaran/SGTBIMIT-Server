170cea13211bb2c3c6c3fd049bf626ae018b22e5
Jatin Ahluwalia <jatin.ahluwalia.5@gmail.com>
2023-05-31 22:02:18 +0530

    fix

--- Controllers/IndustrialVisits.js
+++ Controllers/IndustrialVisits.js
@@ -2,50 +2,50 @@ const IndustrialModel = require('../Models/IndustrialVisits')
 const fs = require('fs');
 
 //add
-const IndustrialVisitsAdd = async(req,res) => {
-    
+const IndustrialVisitsAdd = async (req, res) => {
+
     try {
 
-        const {name, about} = req.fields;
-        const {image, companyImage} = req.files;
+        const { name, about } = req.fields;
+        const { image, companyImage } = req.files;
 
-        if (!name || !about){
+        if (!name || !about) {
             return res.status(201).send({ message: "All fields are required" });
         }
-        if (!image || !companyImage){
+        if (!image || !companyImage) {
             return res.status(201).send({ message: "Image field can not be blank" });
         }
-        if (image && image.size > 1000000 || companyImage && companyImage.size > 1000000){
+        if (image && image.size > 1000000 || companyImage && companyImage.size > 1000000) {
             return res.status(201).send({ message: "Image field can not be blank or should be less 1mb" });
         }
 
         const indusvisit = await new IndustrialModel(req.fields);
 
-        if(image){
+        if (image) {
             indusvisit.image.data = fs.readFileSync(image.path),
-            indusvisit.image.contentType = image.type
+                indusvisit.image.contentType = image.type
         }
 
-        if(companyImage){
+        if (companyImage) {
             indusvisit.image.data = fs.readFileSync(companyImage.path),
-            indusvisit.image.contentType = companyImage.type
+                indusvisit.image.contentType = companyImage.type
         }
 
-          const indus = await indusvisit.save()
-          
-          return res.status(200).send({
-            message : "Created successfully",
-            data : indus
-          });
-          
+        const indus = await indusvisit.save()
+
+        return res.status(200).send({
+            message: "Created successfully",
+            data: indus
+        });
+
     } catch (error) {
         console.log(error);
-       
+
     }
 }
 
 //display
-const IndustrialVisitsDisplay = async(req,res) => {
+const IndustrialVisitsDisplay = async (req, res) => {
 
     try {
 
@@ -55,31 +55,31 @@ const IndustrialVisitsDisplay = async(req,res) => {
             return res.status(201).send("No data found");
         }
 
-           return res.status(200).send(indusvisit);
-        
+        return res.status(200).send(indusvisit);
+
     } catch (error) {
-        
+
     }
 }
 
 
 //image display
-const IndustrialVisitsImageDisplay = async(req,res) => {
+const IndustrialVisitsImageDisplay = async (req, res) => {
 
     try {
 
         const { _id } = req.params;
 
-        const indusvisit = await IndustrialModel.findById({_id});
+        const indusvisit = await IndustrialModel.findById({ _id });
 
         if (indusvisit) {
             res.set("Content-type", indusvisit.image.contentType);
             res.set("Content-type", indusvisit.companyImage.contentType);
-            return res.status(200).send(indusvisit.image.data,indusvisit.companyImage.data);
+            return res.status(200).send(indusvisit.image.data, indusvisit.companyImage.data);
         }
 
         return res.status(201).send("No data found");
-        
+
     } catch (error) {
         console.log(error);
     }
@@ -87,26 +87,26 @@ const IndustrialVisitsImageDisplay = async(req,res) => {
 
 
 //Company image display
-// const IndustrialVisitsCompanyImDaisplay = async(req,res) => {
+const IndustrialVisitsCompanyImDaisplay = async (req, res) => {
 
-//     try {
-//         const { _id } = req.params;
+    try {
+        const { _id } = req.params;
 
-//         const indusvisit = await IndustrialModel.findById({_id});
+        const indusvisit = await IndustrialModel.findById({ _id });
 
-//         if (indusvisit) {
-//             res.set("Content-type", indusvisit.image.contentType)
-//             return res.status(200).send(indusvisit.image.data);
-//         }
+        if (indusvisit) {
+            res.set("Content-type", indusvisit.image.contentType)
+            return res.status(200).send(indusvisit.image.data);
+        }
 
-//         return res.status(201).send("No data found");
-        
-//     } catch (error) {
-//         console.log(error);        
-//     }
-// }
+        return res.status(201).send("No data found");
 
-const IndustrialVisitsDelete = async(req,res) => {
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const IndustrialVisitsDelete = async (req, res) => {
 
     try {
 
@@ -117,14 +117,16 @@ const IndustrialVisitsDelete = async(req,res) => {
             return res.status(201).send("No data found");
         }
 
-        const finalDelete = await IndustrialModel.findByIdAndDelete(indusDelete);        
+        const finalDelete = await IndustrialModel.findByIdAndDelete(indusDelete);
         return res.status(201).send(`${finalDelete.name} is deleted successfully`);
-        
+
     } catch (error) {
         console.log(error);
     }
 }
 
 
-module.exports = {IndustrialVisitsAdd, IndustrialVisitsDisplay, IndustrialVisitsDelete,
-        IndustrialVisitsImageDisplay }
+module.exports = {
+    IndustrialVisitsAdd, IndustrialVisitsDisplay, IndustrialVisitsDelete,
+    IndustrialVisitsImageDisplay, IndustrialVisitsCompanyImDaisplay
+}
